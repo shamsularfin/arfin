73f6badf130af07d257c9a70aa551509122ed0e6
shamsularfin <159010604+shamsularfin@users.noreply.github.com>
2024-03-04 16:49:47 +0600

    Add files via upload

---
+++ home2.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+    <form method="" action="" enctype="">
+        <fieldset>
+            
+<table width ="100" hight="300">
+    <h1>Wclcome x company</h1>
+    
+    <nav align ="right">
+    <a href ="home.html">home</a>
+    <a href ="registration.php" >registration</a>
+    <a href ="login.php">login</a>
+</nav>
+
+</table>
+        </fieldset>
+        
+
+    </form>
+</body>
+</html>
